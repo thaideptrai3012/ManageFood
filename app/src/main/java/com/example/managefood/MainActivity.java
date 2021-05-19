c8fc9ccf2f943a61cb5fa0751e3a24eed00038f4
vundph10997 <duyvu792001@gmail.com>
2021-05-19 10:42:19 +0700

    Sửa gradle ,kết nối với Firebase

--- app/src/main/java/com/example/managefood/MainActivity.java
+++ app/src/main/java/com/example/managefood/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     private Button butonLogin;
     private Button buttonSignup;
@@ -18,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         butonLogin = findViewById(R.id.butonLogin);
         buttonSignup = findViewById(R.id.butonSignup);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
+        myRef.setValue("Hello, World!");
 
         butonLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//            startActivity(intent);
 
         });
 
