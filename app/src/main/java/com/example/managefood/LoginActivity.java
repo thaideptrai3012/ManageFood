94f80a3006912ee47d1723f16eb4403947e09e3e
thaianhtran3012 <72481749+thaideptrai3012@users.noreply.github.com>
2021-05-19 09:55:34 +0700

    thaitaph commit update border and code setting

--- app/src/main/java/com/example/managefood/LoginActivity.java
+++ app/src/main/java/com/example/managefood/LoginActivity.java
@@ -3,12 +3,29 @@ package com.example.managefood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText llemail;
+    private EditText llpassword;
+    private Button butonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        butonLogin = findViewById(R.id.butonLogin);
+        llemail = findViewById(R.id.llemail);
+        llpassword = findViewById(R.id.llpassword);
+
+        butonLogin.setOnClickListener(v -> {
+            if (llemail.getText().toString().isEmpty() || llpassword.getText().toString().isEmpty()) {
+                Toast.makeText(LoginActivity.this,
+                        "Mời bạn nhập email và password!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
