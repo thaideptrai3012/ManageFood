8fc793a0b754c4423741114af2c02eac0bbe017c
vundph10997 <duyvu792001@gmail.com>
2021-05-25 21:19:44 +0700

    - gộp Các activity hiển thị food(Rice,Beverage, ...) thành 1 , Lấy data từ firebase đổ lên Recycle để hiển thị

--- app/src/main/java/com/example/managefood/HomeActivity.java
+++ app/src/main/java/com/example/managefood/HomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,10 +18,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 
-
+import com.example.managefood.Model.Food;
 import com.google.android.material.navigation.NavigationView;
-
-
 
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,6 +29,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     Toolbar toolbar;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
+    ImageView imgFastFood, imgRice, imgHealthy, imgBeverage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +38,12 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout = findViewById(R.id.drawerLayout);
         nav_view = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
+        imgBeverage = findViewById(R.id.drink);
+        imgRice = findViewById(R.id.rice);
+        imgHealthy = findViewById(R.id.healthyFood);
+        imgFastFood = findViewById(R.id.fastFood);
+
+
         setSupportActionBar(toolbar);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
                 R.string.open, R.string.close);
@@ -47,15 +54,45 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         actionBarDrawerToggle.syncState();
         nav_view.setNavigationItemSelectedListener(this);
 
-
-
+        imgFastFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, FoodActivity.class);
+                intent.putExtra("TYPE_FOOD","FastFood");
+                startActivity(intent);
+            }
+        });
+        imgHealthy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, FoodActivity.class);
+                intent.putExtra("TYPE_FOOD","Healthy");
+                startActivity(intent);
+            }
+        });
+        imgRice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, FoodActivity.class);
+                intent.putExtra("TYPE_FOOD","Rice");
+                startActivity(intent);
+            }
+        });
+        imgBeverage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, FoodActivity.class);
+                intent.putExtra("TYPE_FOOD","Beverage");
+                startActivity(intent);
+            }
+        });
     }
 
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        switch (id){
+        switch (id) {
             case R.id.cart:
                 Intent x = new Intent(HomeActivity.this, CartActivity.class);
                 startActivity(x);
