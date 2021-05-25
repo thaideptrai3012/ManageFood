3ae242ff1a007c18221ffe8e8afe38cebfdd9afb
DESKTOP-UDAS293\pc <trananhthai3012@gmail.com>
2021-05-25 16:31:36 +0700

    remove <<< HEAD

--- app/src/main/java/com/example/managefood/CartActivity.java
+++ app/src/main/java/com/example/managefood/CartActivity.java
@@ -10,9 +10,9 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
-<<<<<<< HEAD
-=======
 
->>>>>>> 7618c17ae1be772b5ba7121174be5d70e022f4cd
+
+
+
     }
 }
