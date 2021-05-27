f79fe566efcbb8823f5290a602e834caa58c724b
LongDao29 <Longsama2909@gmail.com>
2021-05-27 16:46:00 +0700

    Long thiết kế màn đổi mật khẩu

--- app/src/main/java/com/example/managefood/ChangePassActivity.java
+++ app/src/main/java/com/example/managefood/ChangePassActivity.java
@@ -3,12 +3,16 @@ package com.example.managefood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ChangePassActivity extends AppCompatActivity {
-
+    EditText oldPassTxt, newPassTxt, xacNhanMatKhauTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_pass);
+        oldPassTxt = findViewById(R.id.oldPassTxt);
+        newPassTxt = findViewById(R.id.newPassTxt);
+        xacNhanMatKhauTxt = findViewById(R.id.xacNhanMatKhauTxt);
     }
 }
