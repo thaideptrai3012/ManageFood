d49fc53f9d2786eb3f4328f07d206addb19dd248
vundph10997 <duyvu792001@gmail.com>
2021-06-05 22:56:34 +0700

    hiển thị danh sách trong giao diện chủ cửa hàng

--- app/src/main/java/com/example/managefood/UpdateFoodActivity.java
+++ app/src/main/java/com/example/managefood/UpdateFoodActivity.java
@@ -2,7 +2,11 @@ package com.example.managefood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.managefood.Model.Food;
 
 public class UpdateFoodActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class UpdateFoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_food);
+
+        //Lấy đối tượng food cần update:
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        Food food = (Food) bundle.getSerializable("FoodUpdate");
+        Log.e("Name",food.getName());
     }
 }
